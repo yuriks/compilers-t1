e0c73fb8684ac55506db0fe6557f3efdc17a6532
yuriks <yuriks@yuriks.net>
2012-05-09 03:08:25 -0300

    Added function to load grammar info from ast.

--- grammar.py
+++ grammar.py
@@ -1,12 +1,28 @@
+import ast
+import ast_simplify
+import lexer
+
 class Grammar:
     def __init__(self):
         self.rules = {}
-        self.token = set()
+        self.tokens = {}
 
         self.rule_unique_nums = {}
         self.token_literals = set()
+
+        self.lexer = None
 
     def generate_temp_name(self, rule_name):
         num = self.rule_unique_nums.get(rule_name, 0) + 1
         self.rule_unique_nums[rule_name] = num
         return '%s$%d' % (rule_name, num)
+
+    def load_from_ast(self, ast_root):
+        for st in ast_root.statements:
+            if isinstance(st, ast.TokenDefinition):
+                self.tokens[st.name] = st.regexp
+            elif isinstance(st, ast.RuleDefinition):
+                self.rules[st.name] = ast_simplify.convert_ast_to_rule(self, st.name, st.expression)
+
+        lexer.add_literal_tokens(self.tokens, self.token_literals)
+        self.lexer = lexer.Tokenizer(self.tokens)
